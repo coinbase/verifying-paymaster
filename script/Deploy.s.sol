a1c949268be97fdba8ba852d31b2af5f73409167
Chris Hunter <chris.hunter@coinbase.com>
2024-09-16 12:06:28 -0400

    Deployment script and addresses

---
+++ script/Deploy.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "forge-std/Script.sol";
+
+import {SafeSingletonDeployer} from "../src/SafeSingletonDeployer.sol";
+import {VerifyingPaymaster} from "../src/VerifyingPaymaster.sol";
+
+contract DeployScript is Script {
+  function run() public {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address entrypoint = vm.envAddress("ENTRYPOINT");
+    address verifyingSigner = vm.envAddress("VERIFYING_SIGNER");
+    address owner = vm.envAddress("OWNER");
+
+    address deploymentAddress = SafeSingletonDeployer.computeAddress({
+        creationCode: type(VerifyingPaymaster).creationCode,
+        args: abi.encode(entrypoint, verifyingSigner, owner),
+        salt: 0x3078636200000000000000000000000000000000000000000000000000000000
+    });
+
+    console2.log("Deployment Address");
+    console2.log(deploymentAddress);
+
+    SafeSingletonDeployer.broadcastDeploy({
+      deployerPrivateKey: deployerPrivateKey,
+      creationCode: type(VerifyingPaymaster).creationCode,
+      args: abi.encode(entrypoint, verifyingSigner, owner),
+      salt: 0x3078636200000000000000000000000000000000000000000000000000000000
+    });
+  }
+}
